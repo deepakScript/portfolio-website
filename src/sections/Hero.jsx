f8dfde645311a703fafd0b8fa2dcc78653bf806f
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2025-09-19 08:29:55 +0545

    hello

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,32 +1,65 @@
-import React, { useRef } from 'react'
-
+import { Canvas } from "@react-three/fiber";
+import { Planet } from "../components/Planet";
+import { Environment, Float, Lightformer } from "@react-three/drei";
+import { useMediaQuery } from "react-responsive";
+import AnimatedHeaderSection from "../components/AnimatedHeaderSection";
 const Hero = () => {
-    const contextRef = useRef(null);
-    const headerRef = useRef(null);
+  const isMobile = useMediaQuery({ maxWidth: 853 });
+  const text = `I help growing brands and startups gain an
+unfair advantage through premium
+results driven webs/apps`;
   return (
-    <section className='flex flex-col justify-end min-h-screen'>
-        <div ref={contextRef}>
-            <div
-            style={{clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%"}}
-            >
-                <div
-                ref={headerRef}
-                className='flex flex-col justify-center gap-12 pt-16 sm:gap-16'>
-                    <p>404 No Bugs Found</p>
-
-                    <div className='px-10'>
-                        <h1 className='flex flex-col flex-wrap gap-12 text-black uppercase banner-text-responsive  
-                        sm:gap-16 md:block
-                        '>Deepak Chhantyal</h1>
-                    </div>
-                </div>
-            </div>
-            <div className='relative px-10 text-black'>
-
-            </div>
-        </div>
+    <section id="home" className="flex flex-col justify-end min-h-screen">
+      <AnimatedHeaderSection
+        subTitle={"404 No Bugs Found"}
+        title={"Ali Sanati"}
+        text={text}
+        textColor={"text-black"}
+      />
+      <figure
+        className="absolute inset-0 -z-50"
+        style={{ width: "100vw", height: "100vh" }}
+      >
+        <Canvas
+          shadows
+          camera={{ position: [0, 0, -10], fov: 17.5, near: 1, far: 20 }}
+        >
+          <ambientLight intensity={0.5} />
+          <Float speed={0.5}>
+            <Planet scale={isMobile ? 0.7 : 1} />
+          </Float>
+          <Environment resolution={256}>
+            <group rotation={[-Math.PI / 3, 4, 1]}>
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[0, 5, -9]}
+                scale={10}
+              />
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[0, 3, 1]}
+                scale={10}
+              />
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[-5, -1, -1]}
+                scale={10}
+              />
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[10, 1, 0]}
+                scale={16}
+              />
+            </group>
+          </Environment>
+        </Canvas>
+      </figure>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
